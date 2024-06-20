f327cd14cd6ce0baed71b2531e23110838ca7ddd
mohammad erfany <98615272+merfdev@users.noreply.github.com>
2024-06-20 11:35:53 +0330

    change serverSide rendering in homePage

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,19 +1,21 @@
 import BuyResidentialsPage from "@/components/template/BuyResidentialsPage";
+import Profile from "@/models/Profile";
+import connectDB from "@/utils/connectDB";
 
 async function BuyResidentials({ searchParams }) {
-  const res = await fetch("http://localhost:3000/api/profile", {
-    cache: "no-store",
-  });
-  console.log(searchParams);
-  const data = await res.json();
-  if (data.error) return <h3>مشکلی پیش آمده</h3>;
-  let fData = data.data;
+  await connectDB();
+  const profile = await Profile.find({ category: searchParams.category });
+  console.log(profile);
 
-  if (searchParams.category) {
-    fData = fData.filter((i) => i.category === searchParams.category);
-  }
+  // const data = await res.json();
+  // if (data.error) return <h3>مشکلی پیش آمده</h3>;
+  // let fData = data.data;
 
-  return <BuyResidentialsPage fdata={fData} />;
+  // if (searchParams.category) {
+  //   fData = fData.filter((i) => i.category === searchParams.category);
+  // }
+
+  return <BuyResidentialsPage fdata={profile} />;
 }
 
 export default BuyResidentials;
